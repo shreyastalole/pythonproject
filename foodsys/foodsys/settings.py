abda13fb76e893972b829bce3b1569a8c37ac426
shreyas talole <shreyastalole@gmail.com>
2019-03-28 01:20:37 +0530

    update

--- foodsys/foodsys/settings.py
+++ foodsys/foodsys/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
-CRISPY_TEMPLATE_PACK ='bootstrap3'
+CRISPY_TEMPLATE_PACK ='bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'
 
